f3ff696d1abc7e35ed737fcc0471b2c6b970febd
Mark Bussard <markbussard@outlook.com>
2021-05-15 23:44:52 -0700

    Update routes

--- source/routers/journalRouter.js
+++ source/routers/journalRouter.js
@@ -11,113 +11,109 @@ const journalController = new JournalController(Journal);
 /* v Routes for getting journal data v */
 
 // Route for getting user's journal
-router.get('/journal/:id', (req, res)=> {
+router.get('/journal/:id', (req, res) => {
   journalController.getJournal(req, res);
 });
 
 // Route for getting journal entry for today's date
-router.get('/journal-entry/:date', (req, res)=> {
+router.get('/journal-entry/:date', (req, res) => {
   journalController.getJournalEntry(req, res);
 });
 
 // Route for getting journal entries for specific date range
-router.get('/journal-entries/:dates', (req, res)=> {
+router.get('/journal-entries/:dates', (req, res) => {
   journalController.getJournalEntries(req, res);
 });
 
 // Route for getting weekly journal entries
-router.get('/weekly-journal-entries/:dates', (req, res)=> {
+router.get('/weekly-journal-entries/:dates', (req, res) => {
   journalController.getWeeklyJournals(req, res);
 });
 
 // Route for getting monthly journal entries
-router.get('/monthly-journal-entries/:dates', (req, res)=> {
+router.get('/monthly-journal-entries/:dates', (req, res) => {
   journalController.getMonthlyJournals(req, res);
 });
 
 /* ^^ Routes for getting journal data ^^ */
 
-
 /* v Routes for moving data to a new journal entry v */
 
 // Route for moving task from one journal entry to another
-router.put('/migrate-journal-task/:id', (req, res)=> {
+router.put('/migrate-journal-task/:id', (req, res) => {
   journalController.migrateTask(req, res);
 });
 
 // Route for moving note from one journal entry to another
-router.put('/migrate-journal-note/:id', (req, res)=> {
+router.put('/migrate-journal-note/:id', (req, res) => {
   journalController.migrateNote(req, res);
 });
 
 // Route for moving event from one journal entry to another
-router.put('/migrate-journal-event/:id', (req, res)=> {
+router.put('/migrate-journal-event/:id', (req, res) => {
   journalController.migrateEvent(req, res);
 });
 
 /* ^^ Routes for moving data to a new journal entry ^^ */
 
-
 /* v Routes for adding data v */
 
 // Route for creating a new journal entry
-router.post('/create-journal-entry/:id', (req, res)=> {
-  journalController.createJournalEntry(req, res)
+router.post('/add-journal-entry/:id', (req, res) => {
+  journalController.addJournalEntry(req, res);
 });
 
 // Route for adding new task
-router.post('/add-task/:id', (req, res)=> {
-  journalController.addTask(req, res)
+router.post('/add-task/:id', (req, res) => {
+  journalController.addTask(req, res);
 });
 
 // Route for adding new note
-router.post('/add-note/:id', (req, res)=> {
-  journalController.addNote(req, res)
+router.post('/add-note/:id', (req, res) => {
+  journalController.addNote(req, res);
 });
 
 // Route for adding new event
-router.post('/add-event', (req, res)=> {
-  journalController.addEvent(req, res)
+router.post('/add-event/:id', (req, res) => {
+  journalController.addEvent(req, res);
 });
 
 /* ^^ Routes for adding data ^^ */
 
-
 /* v Routes for updating data v */
 
 // Route for updating a task
-router.put('/update-task/:id', (req, res)=> {
-  journalController.updateTask(req, res)
+router.post('/update-task/:id', (req, res) => {
+  journalController.updateTask(req, res);
 });
 
 // Route for updating a note
-router.put('/update-note/:id', (req, res)=> {
-  journalController.updateNote(req, res)
+router.put('/update-note/:id', (req, res) => {
+  journalController.updateNote(req, res);
 });
 
 // Route for updating an event
-router.put('/update-event', (req, res)=> {
-  journalController.updateEvent(req, res)
+router.put('/update-event/:id', (req, res) => {
+  journalController.updateEvent(req, res);
 });
 
 /* ^^ Routes for updating data ^^ */
 
-
 /* v Routes for deleting data v */
 
 // Route for deleting a task
-router.post('/delete-task/:id', (req, res)=> {
-  journalController.deleteTask(req, res)
+router.post('/delete-task/:id', (req, res) => {
+  journalController.deleteTask(req, res);
 });
 
 // Route for deleting a note
-router.post('/delete-note/:id', (req, res)=> {
-  journalController.deleteNote(req, res)
+router.post('/delete-note/:id', (req, res) => {
+  journalController.deleteNote(req, res);
 });
 
 // Route for deleting an event
-router.post('/delete-event', (req, res)=> {
-  journalController.deleteEvent(req, res)
+router.post('/delete-event/:id', (req, res) => {
+  journalController.deleteEvent(req, res);
 });
 
 /* ^^ Routes for deleting data ^^ */
