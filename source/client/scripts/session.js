5eec7693498eda0d4e5d2f9da2231e8a52ce18bc
Mark Bussard <markbussard@outlook.com>
2021-05-25 19:57:36 -0700

    Add initial functionality for session script. Handles setting and retrieving user and journal data through local storage. Also handles checking if user is logged in

---
+++ source/client/scripts/session.js
@@ -0,0 +1,43 @@
+const session = {};
+
+/**
+ * Sets the user for this session
+ */
+session.setUser = (user) => {
+  localStorage.setItem('sessionUser', JSON.stringify(user));
+};
+
+/**
+ * Gets session user
+ * @returns {object} User.
+ */
+session.getUser = () => JSON.parse(localStorage.getItem('sessionUser'));
+
+/**
+ * Checks if user is currently logged in
+ * @returns {bool}.
+ */
+session.isUserLoggedIn = () => {
+  const user = JSON.parse(localStorage.getItem('sessionUser'));
+
+  if (user) {
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Sets the journal for this session
+ */
+session.setJournal = (journal) => {
+  localStorage.setItem('userJournal', JSON.stringify(journal));
+};
+
+/**
+ * Gets the journal for this session
+ * @returns {object} User journal.
+ */
+session.getJournal = () => JSON.parse(localStorage.getItem('userJournal'));
+
+export default session;
