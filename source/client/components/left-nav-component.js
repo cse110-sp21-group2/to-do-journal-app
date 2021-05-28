d7320a127efb310d759fa996a22d1b33bd913821
sal-iraqi <60715941+sal-iraqi@users.noreply.github.com>
2021-05-27 18:48:37 -0700

    Update left-nav-component.js

--- source/client/components/left-nav-component.js
+++ source/client/components/left-nav-component.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 class LeftNav extends HTMLElement {
     constructor() {
       super();
@@ -83,6 +85,6 @@ class LeftNav extends HTMLElement {
 }
   
 // Define, instantiate, and add the component to its respective div to the calling document
-document.customElements.define('left-nav-component', LeftNav);
+customElements.define('left-nav-component', LeftNav);
 const comp = document.createElement('left-nav-component');
 document.querySelector('.left-nav').appendChild(comp);
