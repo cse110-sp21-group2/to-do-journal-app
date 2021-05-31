97f57dffe0c0ce3d709c5a01df5ef461840066de
Mark Bussard <markbussard@outlook.com>
2021-05-30 19:40:10 -0700

    Fix merge conflicts

--- source/client/scripts/dashboard.js
+++ source/client/scripts/dashboard.js
@@ -7,21 +7,21 @@ import session from './session.js';
 
 /**
  * TODO:
-    * X GET user
-    * X GET user id
-    * X GET today's date
-    * X GET journal entry with user's id and today's date
-    *  X GET tasks array
-    *  X ITERATE through tasks and grab 'content' and 'date'
-    *  X POST/DISPLAY task's content with dates
-    *
-    *  X GET events array
-    *  X ITERATE through events and grab 'content', 'startTime', and 'endTime'
-    *  X POST/DISPLAY event's content with time
-    *
-    *  X GET notes array
-    *  X ITERATE through notes and grab 'content'
-    *  X POST/DISPLAY note's content
+ * X GET user
+ * X GET user id
+ * X GET today's date
+ * X GET journal entry with user's id and today's date
+ *  X GET tasks array
+ *  X ITERATE through tasks and grab 'content' and 'date'
+ *  X POST/DISPLAY task's content with dates
+ *
+ *  X GET events array
+ *  X ITERATE through events and grab 'content', 'startTime', and 'endTime'
+ *  X POST/DISPLAY event's content with time
+ *
+ *  X GET notes array
+ *  X ITERATE through notes and grab 'content'
+ *  X POST/DISPLAY note's content
  */
 
 // Today's date
@@ -30,7 +30,7 @@ const today = new Date();
 const user = session.getUser();
 // Current user's id
 const id = user._id;
-console.log(`Print current user's id: ${  id}`);
+console.log(`Print current user's id: ${id}`);
 
 console.log("Print current user's id: ", id);
 console.log(today);
@@ -39,49 +39,41 @@ const getEntry = async () => {
   const payload = {
     id,
     today,
-    type: 'Daily'
-  }
+    type: 'Daily',
+  };
   const { entry } = await journalAPI.getJournalEntry(payload);
 
-  return entry
-}
+  return entry;
+};
 
 const entry = getEntry();
 // TEST getting journal entry from DB
 // const entry = await journalAPI.getJournalEntry("60ac3af75cc18f1184f58b9e", "2021-05-24T23:47:03.282+00:00", "Daily");
 
-
 // GET each task from entry and DISPLAY it
-entry.data.tasks.forEach((task) => {
-    const newTask = document.createElement('task-toggle');
 entry.tasks.forEach((task) => {
-    const newTask = document.createElement('task-toggle');
-    newTask.content = task;
-    const taskDate = new Date(task.dueDate);
-    newTask.date = taskDate;
-    document.querySelector(".task-container").appendChild(newTask);
+  const newTask = document.createElement('task-toggle');
+  newTask.content = task;
+  const taskDate = new Date(task.dueDate);
+  newTask.date = taskDate;
+  document.querySelector('.task-container').appendChild(newTask);
 });
 
-// GET each event from entry and DISPLAY it
-entry.data.events.forEach((event) => {
-    const newEvent= document.createElement('event-toggle');
-    const startTime = new Date(event.startTime);
-    const endTime = new Date(event.endTime);
 entry.events.forEach((event) => {
-    const newEvent= document.createElement('event-toggle');
-    const startTime = new Date(event.startTime);
-    const endTime = new Date(event.endTime);
-    newEvent.content = event;
-    newEvent.startTime = startTime;
-    newEvent.endTime = endTime;
-    document.querySelector(".event-container").appendChild(newEvent);
+  const newEvent = document.createElement('event-toggle');
+  const startTime = new Date(event.startTime);
+  const endTime = new Date(event.endTime);
+  newEvent.content = event;
+  newEvent.startTime = startTime;
+  newEvent.endTime = endTime;
+  document.querySelector('.event-container').appendChild(newEvent);
 });
 
 // GET each note from entry and DISPLAY it
 entry.notes.forEach((note) => {
-    const newNote = document.createElement('note-toggle');
-    newNote.content = note;
-    document.querySelector('.today-container').appendChild(newNote);
+  const newNote = document.createElement('note-toggle');
+  newNote.content = note;
+  document.querySelector('.today-container').appendChild(newNote);
 });
 
 // Scroll through tasks, events, and notes with overflow property
