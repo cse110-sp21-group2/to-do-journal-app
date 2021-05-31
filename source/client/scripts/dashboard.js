3f82424c68ca1279a05b277376ff78d8031ab161
Mark Bussard <markbussard@outlook.com>
2021-05-30 19:32:37 -0700

    Fix linting issues

--- source/client/scripts/dashboard.js
+++ source/client/scripts/dashboard.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-console */
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-undef */
-
+/* eslint-disable import/extensions */
 import journalAPI from '../api/journalAPI.js';
 import session from './session.js';
 
-/** 
+/**
  * TODO:
     * X GET user
     * X GET user id
@@ -13,32 +14,47 @@ import session from './session.js';
     *  X GET tasks array
     *  X ITERATE through tasks and grab 'content' and 'date'
     *  X POST/DISPLAY task's content with dates
-    * 
+    *
     *  X GET events array
     *  X ITERATE through events and grab 'content', 'startTime', and 'endTime'
     *  X POST/DISPLAY event's content with time
-    * 
+    *
     *  X GET notes array
     *  X ITERATE through notes and grab 'content'
     *  X POST/DISPLAY note's content
  */
 
 // Today's date
-const today = new Date();    
+const today = new Date();
 // Current User
 const user = session.getUser();
 // Current user's id
 const id = user._id;
+console.log(`Print current user's id: ${  id}`);
+
 console.log("Print current user's id: ", id);
 console.log(today);
 // GET entry promise and set it to JSON
-const entry = journalAPI.getJournalEntry(id, today, "Daily");
+const getEntry = async () => {
+  const payload = {
+    id,
+    today,
+    type: 'Daily'
+  }
+  const { entry } = await journalAPI.getJournalEntry(payload);
+
+  return entry
+}
+
+const entry = getEntry();
 // TEST getting journal entry from DB
 // const entry = await journalAPI.getJournalEntry("60ac3af75cc18f1184f58b9e", "2021-05-24T23:47:03.282+00:00", "Daily");
 
 
 // GET each task from entry and DISPLAY it
 entry.data.tasks.forEach((task) => {
+    const newTask = document.createElement('task-toggle');
+entry.tasks.forEach((task) => {
     const newTask = document.createElement('task-toggle');
     newTask.content = task;
     const taskDate = new Date(task.dueDate);
@@ -48,7 +64,11 @@ entry.data.tasks.forEach((task) => {
 
 // GET each event from entry and DISPLAY it
 entry.data.events.forEach((event) => {
-    const newEvent= document.createElement('event-toggle'); 
+    const newEvent= document.createElement('event-toggle');
+    const startTime = new Date(event.startTime);
+    const endTime = new Date(event.endTime);
+entry.events.forEach((event) => {
+    const newEvent= document.createElement('event-toggle');
     const startTime = new Date(event.startTime);
     const endTime = new Date(event.endTime);
     newEvent.content = event;
@@ -58,7 +78,7 @@ entry.data.events.forEach((event) => {
 });
 
 // GET each note from entry and DISPLAY it
-entry.data.notes.forEach((note) => {
+entry.notes.forEach((note) => {
     const newNote = document.createElement('note-toggle');
     newNote.content = note;
     document.querySelector('.today-container').appendChild(newNote);
