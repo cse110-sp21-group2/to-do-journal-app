667e88750b96b18c84ac615b8cf40652a52428be
sal-iraqi <sshathar@ucsd.edu>
2021-06-07 17:08:32 -0700

    update settings

--- source/client/scripts/settings.js
+++ source/client/scripts/settings.js
@@ -15,21 +15,15 @@ const submitBtn = document.querySelector('.save-btn');
 // Current User
 const user = session.getUser();
 // Current user's id
-//const id = user._id;
-const id = "60b7ec68c67f03bea8ba2fb9";
+const id = user._id;
 
 //Initialize the input fields to display the current user's info
-/*nameInput.value = user.userName;
+nameInput.value = user.userName;
 emailInput.value = user.email;
 termInput = user.term;
 firstDayOfTheWeekInput.value = user.firstDayOfTheWeek;
 stylesInput = user.theme;
-languageInput = user.language;*/
-
-/*nameInput.value = "Salwan";
-emailInput.value = "sshathar@ucsd.edu";
-languageInput.value = "spanish";
-firstDayOfTheWeekInput.value = "tuesday";*/
+languageInput = user.language;
 
 /**
  * Updates the settings of the user
@@ -48,8 +42,6 @@ submitBtn.addEventListener('click', async () => {
   
   // Update the user info in the database
   const updatedResult = await userAPI.updateUserInfo({id, updatedUser});
-
-  console.log(updatedResult);
 
   // Update the user info locally
   session.setUser(updatedResult.data);
