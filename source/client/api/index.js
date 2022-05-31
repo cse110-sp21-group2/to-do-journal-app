373ca2760a147fa645e1eb59e87de8f1fd6572ba
traipham <67611258+traipham@users.noreply.github.com>
2022-05-31 14:22:45 -0700

    heroku push attempt

--- source/client/api/index.js
+++
@@ -1,9 +0,0 @@
-import authAPI from './authAPI';
-import journalAPI from './journalAPI';
-import userAPI from './userAPI';
-
-export default {
-  authAPI,
-  journalAPI,
-  userAPI
-};
--- source/test/example.test.js
+++
@@ -1,8 +0,0 @@
-import { assert } from 'chai'
-import { app } from '../example.js';
-
-describe('App', function() {
-    it('App should return hello', function() {
-        assert.equal(app(), 'hello');
-    })
-})
